ba3af2f5b7c3ee7e97d247974c3de8c0a8b9cb77
Stephen Young <stephen.young.dev@gmail.com>
2022-09-12 20:10:14 -0500

    add set scheduler to generate package

--- parrot_tools/generate/__init__.py
+++ parrot_tools/generate/__init__.py
@@ -1,11 +1,12 @@
 from parrot_tools.generate.settings import BatchSettings, Prompt, RunSettings
-from parrot_tools.generate.stable_diffusion import run_prompts
+from parrot_tools.generate.stable_diffusion import run_prompts, set_scheduler
 from parrot_tools.generate.stable_diffusion_pipeline import (
     StableDiffusionPipelineCustom,
 )
 
 __all__ = [
     "run_prompts",
+    "set_scheduler",
     "Prompt",
     "RunSettings",
     "BatchSettings",
