5657523bc94e939a9259582597d1bb6226352d2a
Stephen Young <stephen.young.dev@gmail.com>
2022-09-15 08:01:49 -0500

    add hybrid studies capabilities

--- parrot_tools/utils/prompt_utils.py
+++ parrot_tools/utils/prompt_utils.py
@@ -1,4 +1,5 @@
-from typing import List
+import itertools
+from typing import Iterator, List
 
 from parrot_tools.generate.settings import Prompt
 from parrot_tools.utils.file_utils import format_base_filename
@@ -16,7 +17,7 @@ def _parse_name(name: str) -> str:
     return name
 
 
-def _format_style_for_prompt(style_name: str) -> str:
+def _format_name_for_prompt(name: str) -> str:
     """Format style name for prompt.
 
     Args:
@@ -25,17 +26,18 @@ def _format_style_for_prompt(style_name: str) -> str:
     Returns:
         str: formatted style name
     """
-    last_first = style_name.strip().split(",")
-    if len(last_first) == 2:
-        last, first = last_first
-        last = last.strip()
-        first = first.strip()
-        if last:
-            return f" by {first.strip()} {last.strip()}"
-        # if no last name, just return the first name
-        return f" by {first}"
-    # if no comma, just return the name
-    return f", {style_name}"
+
+    if "," not in name:
+        return name
+
+    # if comma in name, assume it's a person's name
+    last, first = name.strip().split(",")
+    last = last.strip()
+    first = first.strip()
+    if last:
+        return f"{first.strip()} {last.strip()}"
+    # if no last name, just return the first name
+    return first
 
 
 def prepare_prompts_for_study(
@@ -48,7 +50,8 @@ def prepare_prompts_for_study(
 
     Args:
         base_prompts_block (str): block of prompts separated by newlines
-        styles_block (str): block of styles separated by newlines
+        names_block (str): block of names separated by newlines
+        modifiers_block (str): block of modifiers separated by newlines
         append_to_all_prompts (str): a string to append to all prompts
 
     Returns:
@@ -63,14 +66,98 @@ def prepare_prompts_for_study(
     base_prompts = [x.strip() for x in base_prompts_block.splitlines() if x.strip()]
 
     # build up list of prompts to run
-    prompts_to_run = [
+    artist_prompts = [
         Prompt(
-            folder_name=format_base_filename(a),
-            base_filename=format_base_filename(a),
-            prompt=f"{b}{_format_style_for_prompt(a)}{append_to_all_prompts}",
+            folder_name=format_base_filename(name),
+            base_filename=format_base_filename(name),
+            prompt=f"{b} by {_format_name_for_prompt(name)}{append_to_all_prompts}",
         )
-        for a in names_list + modifiers_list
+        for name in names_list
         for b in base_prompts
     ]
 
-    return prompts_to_run
+    modifier_prompts = [
+        Prompt(
+            folder_name=format_base_filename(modifier),
+            base_filename=format_base_filename(modifier),
+            prompt=f"{b}, {modifier}{append_to_all_prompts}",
+        )
+        for modifier in modifiers_list
+        for b in base_prompts
+    ]
+
+    return artist_prompts + modifier_prompts
+
+
+def unique_combinations(items: List[str], n) -> Iterator[List[str]]:
+    """Generate every possible unique combination of elements in a list.
+
+    Args:
+        iterable (list): list of elements
+        n (int): length of combinations
+
+    Returns:
+        list: list of combinations
+    """
+    for x in itertools.combinations(items, n):
+        yield list(x)
+
+
+def prepare_hybrid_prompts_for_study(
+    base_prompts_block: str,
+    names_block: str,
+    modifiers_block: str,
+    append_to_all_prompts: str,
+    count: int,
+) -> List[Prompt]:
+    """Prepare prompts for study.
+
+    Args:
+        base_prompts_block (str): block of prompts separated by newlines
+        names_block (str): block of names separated by newlines
+        modifiers_block (str): block of modifiers separated by newlines
+        append_to_all_prompts (str): a string to append to all prompts
+        hybrid_count (int): the amount to hybridize per study
+
+    Returns:
+        List[Prompt]: List of prompts to study
+    """
+
+    # clean up the append to all prompts
+    if append_to_all_prompts and not append_to_all_prompts.startswith(","):
+        append_to_all_prompts = ", " + append_to_all_prompts
+
+    # split up the blocks into lists
+    names_list = [_parse_name(x) for x in names_block.splitlines() if x.strip()]
+    modifiers_list = [x.strip() for x in modifiers_block.splitlines() if x.strip()]
+    base_prompts = [x.strip() for x in base_prompts_block.splitlines() if x.strip()]
+
+    # build up list of prompts to run
+    prompts = []
+
+    # add all the artist prompts
+    for names in unique_combinations(names_list, count):
+        formatted_names = [_format_name_for_prompt(name) for name in names]
+        base_filename = format_base_filename("_".join(formatted_names))
+        for b in base_prompts:
+            prompts.append(
+                Prompt(
+                    folder_name=base_filename,
+                    base_filename=base_filename,
+                    prompt=f"{b} by {' and '.join(formatted_names)}{append_to_all_prompts}",
+                )
+            )
+
+    # add all the modifier prompts
+    for modifiers in unique_combinations(modifiers_list, count):
+        for b in base_prompts:
+            base_filename = format_base_filename("_".join(modifiers))
+            prompts.append(
+                Prompt(
+                    folder_name=base_filename,
+                    base_filename=base_filename,
+                    prompt=f"{b}, {', '.join(modifiers)}{append_to_all_prompts}",
+                )
+            )
+
+    return prompts
--- tests/utils/test_prompt_utils.py
+++ tests/utils/test_prompt_utils.py
@@ -1,4 +1,7 @@
-from parrot_tools.utils.prompt_utils import prepare_prompts_for_study
+from parrot_tools.utils.prompt_utils import (
+    prepare_hybrid_prompts_for_study,
+    prepare_prompts_for_study,
+)
 
 
 def test_prepare_prompts_for_study():
@@ -58,3 +61,145 @@ def test_prepare_prompts_for_study():
     assert prompts[12].folder_name == "vaporwave"
     assert prompts[12].base_filename == "vaporwave"
     assert prompts[12].prompt == "This is a test prompt, vaporwave, artstation"
+
+
+def test_prepare_prompts_for_study_with_empty_names_block():
+
+    base_prompts_block = """
+    This is a test prompt
+    This is another test prompt"""
+
+    names_block = ""
+
+    modifiers_block = """
+    synthwave
+    """
+
+    append_to_all_prompts = "artstation"
+    prompts = prepare_prompts_for_study(
+        base_prompts_block, names_block, modifiers_block, append_to_all_prompts
+    )
+
+    assert len(prompts) == 2
+    assert prompts[0].folder_name == "synthwave"
+    assert prompts[0].base_filename == "synthwave"
+    assert prompts[0].prompt == "This is a test prompt, synthwave, artstation"
+    assert prompts[1].prompt == "This is another test prompt, synthwave, artstation"
+
+
+def test_prepare_prompts_for_study_with_empty_modifiers_block():
+
+    base_prompts_block = """
+    This is a test prompt
+    This is another test prompt"""
+
+    names_block = """
+    Aivazovsky, Ivan
+    """
+
+    modifiers_block = ""
+
+    append_to_all_prompts = "artstation"
+
+    prompts = prepare_prompts_for_study(
+        base_prompts_block, names_block, modifiers_block, append_to_all_prompts
+    )
+
+    assert len(prompts) == 2
+    assert prompts[0].folder_name == "Aivazovsky_Ivan"
+    assert prompts[0].base_filename == "Aivazovsky_Ivan"
+    assert prompts[0].prompt == "This is a test prompt by Ivan Aivazovsky, artstation"
+    assert (
+        prompts[1].prompt
+        == "This is another test prompt by Ivan Aivazovsky, artstation"
+    )
+
+
+def test_prepare_prompts_for_study_with_empty_append_to_all_prompts():
+
+    base_prompts_block = """
+    This is a test prompt
+    This is another test prompt"""
+
+    names_block = """
+    Aivazovsky, Ivan
+    """
+
+    modifiers_block = ""
+
+    append_to_all_prompts = ""
+
+    prompts = prepare_prompts_for_study(
+        base_prompts_block, names_block, modifiers_block, append_to_all_prompts
+    )
+
+    assert len(prompts) == 2
+    assert prompts[0].folder_name == "Aivazovsky_Ivan"
+    assert prompts[0].base_filename == "Aivazovsky_Ivan"
+    assert prompts[0].prompt == "This is a test prompt by Ivan Aivazovsky"
+    assert prompts[1].prompt == "This is another test prompt by Ivan Aivazovsky"
+
+
+def test_prepare_hybrid_prompts_for_study():
+
+    base_prompts_block = """
+    This is a test prompt
+    This is another test prompt
+    """
+
+    names_block = """
+    Aivazovsky, Ivan
+    RHADS
+    """
+
+    modifiers_block = ""
+
+    append_to_all_prompts = "artstation"
+
+    prompts = prepare_hybrid_prompts_for_study(
+        base_prompts_block, names_block, modifiers_block, append_to_all_prompts, count=2
+    )
+
+    assert len(prompts) == 2
+    assert prompts[0].folder_name == "Ivan_Aivazovsky_RHADS"
+    assert prompts[0].base_filename == "Ivan_Aivazovsky_RHADS"
+    assert (
+        prompts[0].prompt
+        == "This is a test prompt by Ivan Aivazovsky and RHADS, artstation"
+    )
+    assert (
+        prompts[1].prompt
+        == "This is another test prompt by Ivan Aivazovsky and RHADS, artstation"
+    )
+
+
+def test_prepare_hybrid_prompts_for_study():
+
+    base_prompts_block = """
+    This is a test prompt
+    This is another test prompt
+    """
+
+    names_block = ""
+
+    modifiers_block = """
+    synthwave
+    vaporwave
+    """
+
+    append_to_all_prompts = "artstation"
+
+    prompts = prepare_hybrid_prompts_for_study(
+        base_prompts_block, names_block, modifiers_block, append_to_all_prompts, count=2
+    )
+
+    assert len(prompts) == 2
+    assert prompts[0].folder_name == "synthwave_vaporwave"
+    assert prompts[0].base_filename == "synthwave_vaporwave"
+    assert (
+        prompts[0].prompt == "This is a test prompt, synthwave, vaporwave, artstation"
+    )
+    assert (
+        prompts[1].prompt
+        == "This is another test prompt, synthwave, vaporwave, artstation"
+    )
