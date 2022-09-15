9d13661162343173e3052043a4937890c153a987
Stephen Young <stephen.young.dev@gmail.com>
2022-09-15 08:07:45 -0500

    add hybrid to init

--- parrot_tools/utils/__init__.py
+++ parrot_tools/utils/__init__.py
@@ -1,5 +1,13 @@
 from parrot_tools.utils.file_utils import get_abs_filepath
 from parrot_tools.utils.image_utils import make_image_grids
-from parrot_tools.utils.prompt_utils import prepare_prompts_for_study
+from parrot_tools.utils.prompt_utils import (
+    prepare_hybrid_prompts_for_study,
+    prepare_prompts_for_study,
+)
 
-__all__ = ["get_abs_filepath", "make_image_grids", "prepare_prompts_for_study"]
+__all__ = [
+    "get_abs_filepath",
+    "make_image_grids",
+    "prepare_hybrid_prompts_for_study",
+    "prepare_prompts_for_study",
+]
