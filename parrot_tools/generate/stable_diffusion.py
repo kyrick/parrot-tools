9148c9af52eb7005d6eb9b96df3ed25dc655d443
Stephen Young <stephen.young.dev@gmail.com>
2022-11-27 12:52:07 -0600

    fix generation

--- parrot_tools/generate/stable_diffusion.py
+++ parrot_tools/generate/stable_diffusion.py
@@ -5,6 +5,7 @@ from random import randint
 from typing import Any, Dict, List, Optional, Tuple
 
 import torch
+from diffusers.pipelines.stable_diffusion import StableDiffusionPipelineOutput
 from diffusers.schedulers.scheduling_ddim import DDIMScheduler
 from diffusers.schedulers.scheduling_lms_discrete import LMSDiscreteScheduler
 from diffusers.schedulers.scheduling_pndm import PNDMScheduler
@@ -46,7 +47,7 @@ def set_scheduler(pipe, scheduler: SchedulerType):
         raise ValueError(f"Unknown scheduler {scheduler}")
 
 
-def generate_image(
+def generate_images(
     pipe,
     *,
     prompt: str,
@@ -59,7 +60,7 @@ def generate_image(
     init_strength: float = 0.7,
     init_max_pixels: int = 262144,
     num_images_per_prompt: int = 1,
-) -> Dict[str, Any]:
+) -> StableDiffusionPipelineOutput:
     generator = torch.Generator("cuda").manual_seed(seed)
 
     with autocast("cuda"):
@@ -77,46 +78,6 @@ def generate_image(
         )
 
     return res
-
-
-def generate_image_with_retries(
-    pipe,
-    *,
-    prompt: str,
-    start_seed: int,
-    steps: int,
-    cfg_scale: float,
-    height: int,
-    width: int,
-    init_image: Optional[Image.Image] = None,
-    init_strength: float = 0.7,
-    init_max_pixels: int = 262144,
-    retry: int = 0,
-    num_images_per_prompt: int = 1,
-) -> Tuple[list[Image.Image], list[int]]:
-    image = Image.new("RGB", (width, height), "black")
-    final_seed = start_seed
-    for i in range(start_seed, start_seed + 1 + retry):
-        res = generate_image(
-            pipe,
-            prompt=prompt,
-            seed=i,
-            steps=steps,
-            cfg_scale=cfg_scale,
-            height=height,
-            width=width,
-            init_image=init_image,
-            init_strength=init_strength,
-            init_max_pixels=init_max_pixels,
-            num_images_per_prompt=num_images_per_prompt,
-        )
-        images = res.images
-        final_seeds = [i + j for j in range(len(images))]
-        if res.nsfw_content_detected is None:
-            return image, final_seed
-        print("retried!", i)
-
-    return images, final_seeds
 
 
 def get_run_id(settings_folder: Path) -> int:
@@ -162,10 +123,10 @@ def run_prompts(pipe, prompts: List[Prompt], batch_settings: BatchSettings):
             if prompt.init_image is not None:
                 init_image = Image.open(prompt.init_image)
 
-            res_image, final_seeds = generate_image_with_retries(
+            res = generate_images(
                 pipe,
                 prompt=prompt.prompt,
-                start_seed=seed,
+                seed=seed,
                 steps=settings.batch.steps,
                 cfg_scale=settings.batch.cfg_scale,
                 height=settings.batch.image_h,
@@ -173,24 +134,29 @@ def run_prompts(pipe, prompts: List[Prompt], batch_settings: BatchSettings):
                 init_image=init_image,
                 init_strength=prompt.init_strength,
                 init_max_pixels=settings.batch.init_max_pixels,
-                retry=settings.batch.NSFW_retry,
                 num_images_per_prompt=settings.batch.num_images_per_prompt,
             )
-            images += res_image
+            res_images = res.images
+            final_seeds = [
+                seed + j for j in range(settings.batch.num_images_per_prompt)
+            ]
+            images += res_images
 
             if settings.batch.display_individual_images:
-                for image, seed in zip(res_image, final_seeds):
+                for image, seed in zip(res_images, final_seeds):
                     print(f"seed={seed}")
                     display(image)
-            for image, seed in zip(res_image, final_seeds):
-                image.save(settings.get_image_path(run_id=run_id, image_num=i, seed=seed))
+            for image, seed in zip(res_images, final_seeds):
+                image.save(
+                    settings.get_image_path(run_id=run_id, image_num=i, seed=seed)
+                )
 
             # we increment the seed for the next iteration
             seed += settings.batch.num_images_per_prompt
 
         if settings.batch.make_grid:
             # limit to batch size
-            images = images[:settings.batch.batch_size]
+            images = images[: settings.batch.batch_size]
 
             grids = make_image_grids(
                 images,
