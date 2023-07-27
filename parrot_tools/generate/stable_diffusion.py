c71fe03fdb3672a7555b8ffc97f8543324872444
Stephen Young <stephen.young.dev@gmail.com>
2023-07-26 22:06:45 -0500

    update for sdxl

--- parrot_tools/generate/stable_diffusion.py
+++ parrot_tools/generate/stable_diffusion.py
@@ -2,13 +2,9 @@ import os
 from glob import glob
 from pathlib import Path
 from random import randint
-from typing import Any, Dict, List, Optional, Tuple
 
 import torch
 from diffusers.pipelines.stable_diffusion import StableDiffusionPipelineOutput
-from diffusers.schedulers.scheduling_ddim import DDIMScheduler
-from diffusers.schedulers.scheduling_lms_discrete import LMSDiscreteScheduler
-from diffusers.schedulers.scheduling_pndm import PNDMScheduler
 from IPython.display import display  # type: ignore
 from PIL import Image
 from torch import autocast
@@ -18,33 +14,8 @@ from parrot_tools.generate.settings import (
     BatchSettings,
     Prompt,
     RunSettings,
-    SchedulerType,
 )
 from parrot_tools.utils.image_utils import make_image_grids
-
-
-def set_scheduler(pipe, scheduler: SchedulerType):
-    if scheduler == SchedulerType.DDIM:
-        ddim = DDIMScheduler(
-            beta_start=0.00085,
-            beta_end=0.012,
-            beta_schedule="scaled_linear",
-            clip_sample=False,
-            set_alpha_to_one=False,
-        )
-        pipe.scheduler = ddim
-    elif scheduler == SchedulerType.K_LMS:
-        lms = LMSDiscreteScheduler(
-            beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear"
-        )
-        pipe.scheduler = lms
-    elif scheduler == SchedulerType.PNDM:
-        pndm = PNDMScheduler(
-            beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear"
-        )
-        pipe.scheduler = pndm
-    else:
-        raise ValueError(f"Unknown scheduler {scheduler}")
 
 
 def generate_images(
@@ -56,26 +27,19 @@ def generate_images(
     cfg_scale: float,
     height: int,
     width: int,
-    init_image: Optional[Image.Image] = None,
-    init_strength: float = 0.7,
-    init_max_pixels: int = 262144,
     num_images_per_prompt: int = 1,
 ) -> StableDiffusionPipelineOutput:
     generator = torch.Generator("cuda").manual_seed(seed)
 
-    with autocast("cuda"):
-        res = pipe(
-            prompt,
-            num_inference_steps=steps,
-            guidance_scale=cfg_scale,
-            generator=generator,
-            height=height,
-            width=width,
-            init_image=init_image,
-            init_strength=init_strength,
-            init_max_pixels=init_max_pixels,
-            num_images_per_prompt=num_images_per_prompt,
-        )
+    res = pipe(
+        prompt,
+        num_inference_steps=steps,
+        guidance_scale=cfg_scale,
+        generator=generator,
+        height=height,
+        width=width,
+        num_images_per_prompt=num_images_per_prompt,
+    )
 
     return res
 
@@ -92,7 +56,7 @@ def get_run_id(settings_folder: Path) -> int:
     return 0
 
 
-def run_prompts(pipe, prompts: List[Prompt], batch_settings: BatchSettings):
+def run_prompts(pipe, prompts: list[Prompt], batch_settings: BatchSettings):
 
     # loop through prompts and generate images!
     for prompt in prompts:
@@ -117,12 +81,6 @@ def run_prompts(pipe, prompts: List[Prompt], batch_settings: BatchSettings):
 
         images = []
         for i in trange(settings.batch.batch_size):
-
-            init_image = None
-            # if there is an init image, we load it
-            if prompt.init_image is not None:
-                init_image = Image.open(prompt.init_image)
-
             res = generate_images(
                 pipe,
                 prompt=prompt.prompt,
@@ -131,9 +89,6 @@ def run_prompts(pipe, prompts: List[Prompt], batch_settings: BatchSettings):
                 cfg_scale=settings.batch.cfg_scale,
                 height=settings.batch.image_h,
                 width=settings.batch.image_w,
-                init_image=init_image,
-                init_strength=prompt.init_strength,
-                init_max_pixels=settings.batch.init_max_pixels,
                 num_images_per_prompt=settings.batch.num_images_per_prompt,
             )
             res_images = res.images
